357f3b45933a2d8851394c2c16909e572c69cdb6
Antony Jones <aj@desirableobjects.co.uk>
2019-04-01 19:47:07 +0100

    import node fetch correctly for node

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
 'use strict'
 
-const { Api } = require('./lib/api')
-const errors = require('./lib/errors')
-const pkg = require('./package.json')
-
-let config
-
-function configure (options) {
-  config = options
-}
-
-function create (provider) {
-  if (!config) { throw new Error(`${pkg.name} must be configured`) }
-  return new Api(config, provider)
-}
-
-module.exports = { configure, create, errors }
+module.exports = require('./lib/entrypoint')
