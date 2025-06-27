7c5530644dbf5e2667c4db898baf001222e19cd8
Puru Vijay <devpuruvj@gmail.com>
2025-06-27 18:54:35 +0530

    fix: update tsup.config.js to include both esm and cjs formats

--- tsup.config.js
+++ tsup.config.js
@@ -3,18 +3,18 @@ import { defineConfig } from 'tsup'
 export default defineConfig([
   {
     entry: [ 'lib/index.js' ],
-    format: [ 'esm' ],
+    format: [ 'esm', 'cjs' ],
     target: 'es2023',
     dts: true,
     sourcemap: false,
     clean: true
   },
-  {
-    entry: [ 'lib/index.cjs' ],
-    format: [ 'cjs' ],
-    target: 'es2023',
-    dts: true,
-    sourcemap: false,
-    clean: true
-  }
+  // {
+  //   entry: [ 'lib/index.cjs' ],
+  //   format: [ 'cjs' ],
+  //   target: 'node18',
+  //   dts: true,
+  //   sourcemap: false,
+  //   clean: true
+  // }
 ])
